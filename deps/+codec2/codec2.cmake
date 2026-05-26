b106bca58da2bc2b6b919a54b9b0a4d7f4ba6f2d
Vojtech Bubnik <bubnikv@gmail.com>
2026-05-26 11:25:13 +0200

    Fixing codec2 compilation on Windows on ARM

--- deps/+codec2/codec2.cmake
+++ deps/+codec2/codec2.cmake
@@ -20,9 +20,76 @@
 
 set(_codec2_compiler_args "")
 if (MSVC)
+    set(_codec2_clang_hints "")
+    if (DEFINED ENV{VCINSTALLDIR})
+        set(_codec2_vs_host_arch "$ENV{VSCMD_ARG_HOST_ARCH}")
+        if (NOT _codec2_vs_host_arch)
+            if (CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "^[Aa][Rr][Mm]64|^[Aa][Aa][Rr][Cc][Hh]64")
+                set(_codec2_vs_host_arch arm64)
+            else ()
+                set(_codec2_vs_host_arch x64)
+            endif ()
+        endif ()
+        list(APPEND _codec2_clang_hints "$ENV{VCINSTALLDIR}/Tools/Llvm/${_codec2_vs_host_arch}/bin")
+    endif ()
+
+    find_program(_codec2_clang_cl NAMES clang-cl clang-cl.exe HINTS ${_codec2_clang_hints})
+    if (NOT _codec2_clang_cl)
+        message(FATAL_ERROR
+            "codec2 requires clang-cl on Windows because upstream uses C99 VLAs. "
+            "Install the Visual Studio C++ Clang tools for Windows component.")
+    endif ()
+
+    set(_codec2_target_arch "")
+    foreach(_codec2_arch_probe
+            "${CMAKE_GENERATOR_PLATFORM}"
+            "${CMAKE_VS_PLATFORM_NAME}"
+            "${CMAKE_C_COMPILER_ARCHITECTURE_ID}"
+            "${CMAKE_CXX_COMPILER_ARCHITECTURE_ID}"
+            "$ENV{VSCMD_ARG_TGT_ARCH}"
+            "${CMAKE_SYSTEM_PROCESSOR}")
+        if (_codec2_arch_probe MATCHES "^[Aa][Rr][Mm]64[Ee][Cc]")
+            set(_codec2_target_arch ARM64EC)
+            break()
+        elseif (_codec2_arch_probe MATCHES "^([Aa][Rr][Mm]64|[Aa][Aa][Rr][Cc][Hh]64)")
+            set(_codec2_target_arch ARM64)
+            break()
+        elseif (_codec2_arch_probe MATCHES "^[Aa][Rr][Mm]")
+            set(_codec2_target_arch ARM)
+            break()
+        elseif (_codec2_arch_probe MATCHES "^([Ww][Ii][Nn]32|[Xx]86)")
+            set(_codec2_target_arch x86)
+            break()
+        elseif (_codec2_arch_probe MATCHES "^([Xx]64|[Aa][Mm][Dd]64)")
+            set(_codec2_target_arch x64)
+            break()
+        endif ()
+    endforeach()
+
+    if (NOT _codec2_target_arch AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(_codec2_target_arch x64)
+    elseif (NOT _codec2_target_arch)
+        set(_codec2_target_arch x86)
+    endif ()
+
+    if (_codec2_target_arch STREQUAL "ARM64EC")
+        set(_codec2_clang_target arm64ec-pc-windows-msvc)
+    elseif (_codec2_target_arch STREQUAL "ARM64")
+        set(_codec2_clang_target aarch64-pc-windows-msvc)
+    elseif (_codec2_target_arch STREQUAL "ARM")
+        set(_codec2_clang_target armv7-pc-windows-msvc)
+    elseif (_codec2_target_arch STREQUAL "x64")
+        set(_codec2_clang_target x86_64-pc-windows-msvc)
+    else ()
+        set(_codec2_clang_target i686-pc-windows-msvc)
+    endif ()
+
     set(_codec2_compiler_args
-        -DCMAKE_C_COMPILER:STRING=clang-cl
-        -DCMAKE_CXX_COMPILER:STRING=clang-cl)
+        "-DCMAKE_C_COMPILER:FILEPATH=${_codec2_clang_cl}"
+        "-DCMAKE_CXX_COMPILER:FILEPATH=${_codec2_clang_cl}"
+        -DCMAKE_C_COMPILER_TARGET:STRING=${_codec2_clang_target}
+        -DCMAKE_CXX_COMPILER_TARGET:STRING=${_codec2_clang_target})
+    message(STATUS "codec2: using ${_codec2_clang_cl} for ${_codec2_clang_target}")
 endif ()
 
 add_cmake_project(codec2
