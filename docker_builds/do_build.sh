415a2e4c0472f7f73be4fdaa2533a3058c1c48b7
Vojtech Bubnik <bubnikv@gmail.com>
2026-05-30 12:56:09 +0200

    fixed missing liborc on old linux distros

--- docker_builds/do_build.sh
+++ docker_builds/do_build.sh
@@ -17,7 +17,7 @@ fi
 
 # Install dependencies and tools
 apt-get -o Acquire::Retries=3 update
-apt-get -o Acquire::Retries=3 install -y build-essential cmake git pkg-config libfftw3-dev libglfw3-dev ${VOLK_PACKAGE} libzstd-dev libcurl4-openssl-dev libairspy-dev libairspyhf-dev \
+apt-get -o Acquire::Retries=3 install -y build-essential cmake git pkg-config libfftw3-dev libglfw3-dev ${VOLK_PACKAGE} liborc-0.4-dev libzstd-dev libcurl4-openssl-dev libairspy-dev libairspyhf-dev \
     libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev libbladerf-dev liblimesuite-dev p7zip-full wget portaudio19-dev \
     libcodec2-dev autoconf libtool xxd libspdlog-dev ${EXTRA_APT}
 
